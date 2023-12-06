6eb7f2d4a533eed855efeac115d04a87a629d9a0
Weslley Mattos <paocomovoxs@hotmail.com>
2023-12-06 04:02:15 -0300

    servidor local criado

--- server/config.js
+++ server/config.js
@@ -2,21 +2,20 @@ var config = {
     dev: {
         url: 'http://localhost/',
         port: 3000,
-        ambiente: 'DEV',
+        ambiente: 'DEV', 
         database: {
             host: '127.0.0.1',
             port: 3306,
             user: 'root',
             password: 'root',
-            database: 'bd'
+            database: 'pizzaria'
         }
     }
 }
 
-
 exports.get = function get(ambiente) {
 
-    if (ambiente.tolowerCase() === 'dev') {
+    if (ambiente.toLowerCase() === 'dev') {
         return config.dev
     }
 
